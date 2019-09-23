45bc430db37215f499000c44150a0a9191bdf115
Toan Nguyen <toanguyen@zendesk.com>
2019-09-23 11:14:43 +1000

    Use HMAC

--- client.rb
+++ client.rb
@@ -1,16 +1,20 @@
 require 'net/http'
 require 'uri'
 require_relative './request_signing'
-require 'securerandom'
+require 'digest'
+require 'openssl'
+require 'base64'
 
 # uri = URI('https://support.zd-dev.com/api/services/zis/configs/salesforce?scope=*')
 uri = URI('http://localhost:1111/ping')
 request = Net::HTTP::Get.new(uri)
 
-secret_id = "zis_engine123" # KRAGLE_OAUTH_SECRET
-secret_key = "abc1234" # KRAGLE_OAUTH_KEY
 
-auth_header = "tacos is the password"
+secret_key = "abc1234"
+password = "tacos is the password"
+algorithm = OpenSSL::Digest::SHA256.new
+
+auth_header = Base64.strict_encode64(OpenSSL::HMAC.digest(algorithm, secret_key, password))
 request['Authorization'] = auth_header
 puts auth_header
 
--- doorman_server.rb
+++ doorman_server.rb
@@ -1,8 +1,12 @@
 require 'sinatra'
 require_relative 'request_signing'
 
+secret_key = "abc1234"
+password = "tacos is the password"
+algorithm = OpenSSL::Digest::SHA256.new
+
 before do
-  if request.env['HTTP_AUTHORIZATION'] != "tacos is the password"
+  if request.env['HTTP_AUTHORIZATION'] != Base64.strict_encode64(OpenSSL::HMAC.digest(algorithm, secret_key, password))
     halt 401
   end
 end
