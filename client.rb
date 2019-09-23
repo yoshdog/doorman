e5684c50830f99fd5d63df190b3cc5089de5b304
Toan Nguyen <toanguyen@zendesk.com>
2019-09-23 11:26:00 +1000

    Simple client server

--- client.rb
+++ client.rb
@@ -1,18 +1,9 @@
 require 'net/http'
 require 'uri'
-require_relative './simple_request_signing'
-require 'digest'
-require 'openssl'
-require 'base64'
 
 # uri = URI('https://support.zd-dev.com/api/services/zis/configs/salesforce?scope=*')
 uri = URI('http://localhost:1111/ping')
 request = Net::HTTP::Get.new(uri)
-
-secret_key = "abc1234"
-auth_header = SimpleRequestSigning.sign("GET", uri, secret_key)
-request['Authorization'] = auth_header
-puts auth_header
 
 response = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') do |http|
   http.request(request)
--- doorman_server.rb
+++ doorman_server.rb
@@ -1,13 +1,4 @@
 require 'sinatra'
-require_relative 'simple_request_signing'
-require 'uri'
-
-secret_key = "abc1234"
-before do
-  if request.env['HTTP_AUTHORIZATION'] != SimpleRequestSigning.sign(request.request_method, URI(request.url), secret_key)
-    halt 401
-  end
-end
 
 %i(get post put delete patch).each do |method|
    send method, '/*' do
